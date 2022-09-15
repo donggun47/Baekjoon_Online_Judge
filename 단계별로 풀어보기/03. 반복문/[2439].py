dc242ad3db5d62120bb768e254ab20118f4009a9
donggun47 <103362628+donggun47@users.noreply.github.com>
2022-09-16 00:22:10 +0900

    Create [2439].py

---
+++ 단계별로 풀어보기/03. 반복문/[2439].py
@@ -0,0 +1,7 @@
+T=int(input())
+for i in range(T) : print(" "*(T+~i)+"*"*-~i)
+  
+#N=i=int(input())
+#while 0<i:i-=1;print(" "*i+"*"*(N-i))
+
+#s,n='',int(input());exec("s+='*';print(f'%{n}s'%s);"*n)
